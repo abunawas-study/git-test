601cd48ee000a4f07da147eccb1ffdada214931e
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-10-10 20:00:36 +0000

    challenge 2 updated

--- js-basics/intro.js
+++ js-basics/intro.js
@@ -12,3 +12,14 @@ console.log(marksBMI, johnBMI);
 
 let markHigherBMI =  marksBMI > johnBMI;
 console.log(markHigherBMI);
+
+//coding challenge #2 @ javascript fundamentals - part 1
+
+if (marksBMI > johnBMI){
+    console.log("Mark's BMI is Higher than John's")
+    console.log("Mark's BMI is " , "(" + marksBMI + ") Higher than John's BMI" , "(" + johnBMI + ") !"); 
+}
+else{
+    console.log("John's BMI is Higher than Mark's")
+    console.log("John's BMI is " , "(" + johnBMI + ") Higher than Mark's BMI" , "(" + marksBMI + ") !"); 
+}
