dccfbd170f773b9b8c6f981983b333ccace80271
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-29 19:39:27 +0000

    Experimenting with react-router

--- js-basics/main.js
+++ js-basics/main.js
@@ -8,24 +8,29 @@ function processingRequest(callback){
 processingRequest(() =>{
     console.log('Request Completed !!!!')
 });
+processingRequest();
 
 // Exercise 2 — Delayed Execution With setTimeout
 
 function waitAndRun(callback){
     setTimeout(() =>{
-        console.log('Done Waiting');
+        callback();
     }, 2000);
 }
-waitAndRun();
+waitAndRun(() => {
+    console.log('Done waiting !!')
+});
 
 // Exercise 3 — Callback With Data
 function getUser(callback){
     setTimeout(() => {
         let UserObject = {id: 1, name: 'Sadiq'}
-        console.log(UserObject);
+        callback(UserObject);
     }, 1000);
 }
-getUser();
+getUser((data) =>{
+    console.log(data);
+});
 
 //PROMISE PRACTICE PROBLEMS
 
@@ -51,3 +56,57 @@ let MyPromise = new Promise((resolve, reject) => {
 MyPromise
 .then(result => console.log(result))
 .catch(error => console.log(error));
+
+// ASYNC / AWAIT PRACTICE PROBLEMS
+
+// await a Promise 
+async function chores(){
+    let chore = await new Promise(resolve => {
+       setTimeout(() => resolve('Done'), 2000);
+    });
+    console.log(chore);
+}
+chores();
+
+// Fetch API Data With async/await
+async function getUser() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/users');
+    let data = await response.json();
+    console.log(data);   
+} 
+// async/await With Error Handling
+async function loadData(){
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    try{
+        const response = await fetch (url);
+        if (! response.ok){
+            throw new Error (`Response Status: ${response.status}`);
+        }
+        const result  = await response.json();
+        console.log(result);
+    }
+    catch(error){
+        console.error('Something went wrong:', error.message);
+    }
+}
+loadData();
+
+//Run Async Tasks in Order
+async function steps() {
+    let step1 = await new Promise (resolve =>{
+        setTimeout(() => resolve('step 1 done'), 1000);
+    });
+    let step2 = await new Promise (resolve =>{
+        setTimeout(() => resolve('step 2 done'), 1500);
+    });
+    let step3 = await new Promise (resolve =>{
+        setTimeout(() => resolve('step 3 done'), 1000);
+    });
+
+    console.log(step1);
+    console.log(step2);
+    console.log(step3);
+}
+steps();
+
+//Combine Two API Calls Using async/await
